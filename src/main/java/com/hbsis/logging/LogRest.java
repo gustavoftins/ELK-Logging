a4db7f5b1b6790d4bed90b420c27a69c921513c8
matheus.toniotti <matheus.toniotti@gmail.com>
2020-03-05 12:55:43 -0300

    uau2

--- src/main/java/com/hbsis/logging/LogRest.java
+++ src/main/java/com/hbsis/logging/LogRest.java
@@ -3,8 +3,11 @@ package com.hbsis.logging;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api")
@@ -12,8 +15,9 @@ public class LogRest {
     private static final Logger LOGGER = LoggerFactory.getLogger(LogRest.class);
 
     @GetMapping("/log")
-    public String log(){
-        LOGGER.info("este log é legal");
+    public String log(HttpServletRequest r){
+        LOGGER.info(r.getRequestURI());
         return "Retorno";
     }
+
 }
