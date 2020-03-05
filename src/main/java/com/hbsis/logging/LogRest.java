29ee7ee52c79e7c04c83cab2de2b594a96cbe440
matheus.toniotti <matheus.toniotti@gmail.com>
2020-03-05 15:57:19 -0300

    uau2

--- src/main/java/com/hbsis/logging/LogRest.java
+++ src/main/java/com/hbsis/logging/LogRest.java
@@ -2,12 +2,16 @@ package com.hbsis.logging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api")
@@ -18,6 +22,19 @@ public class LogRest {
     public String log(HttpServletRequest r){
         LOGGER.info(r.getRequestURI());
         return "Retorno";
+    }
+
+    @GetMapping("/teste")
+    public void testeElastic(){
+        WebClient web = WebClient.create("http://localhost:9200");
+        WebClient.RequestBodySpec req = web.method(HttpMethod.GET).uri("/logapp/_search?q=tag:log");
+
+        String response2 = req.exchange()
+                .block()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println(response2);
     }
 
 }
