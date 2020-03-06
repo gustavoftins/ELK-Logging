ab0f796ddeaebddd629ae9ee72527adf6eb5893b
matheus.toniotti <matheus.toniotti@gmail.com>
2020-03-06 15:47:24 -0300

    resolvendo problema da socket fechar. o outputstream estava dentro de um try-with-resources fechando a socket.

--- src/main/java/com/hbsis/logging/socket/SocketServer.java
+++ src/main/java/com/hbsis/logging/socket/SocketServer.java
@@ -13,31 +13,29 @@ import java.util.EmptyStackException;
 @Service
 public abstract class SocketServer {
 
-    public static final int SOCKET_PORT = 5501;
+    public static final int SOCKET_PORT = 9768;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SocketServer.class);
 
     public static void streamFile() throws IOException, InterruptedException {
         int ioExceptionErrorCount = 0;
-
-        try (Socket sock = new Socket("localhost", SOCKET_PORT)) {
-            LOGGER.info("Inicinaod socket.");
-            while (true) {
-                try (OutputStream os = sock.getOutputStream()) {
-                    byte[] logBytes = LogStack.stack.pop().getBytes();
-                    os.write(logBytes, 0, logBytes.length);
-                    os.flush();
-                } catch (EmptyStackException e) {
-                    continue;
-                }catch (IOException io){
-                    if(ioExceptionErrorCount == 3){LOGGER.error("Erro ao pegar OutputStream da socket. \n{}", io); break;}
-                    ioExceptionErrorCount++;
-                    Thread.sleep(1000);
-                    continue;
+        Socket sock = new Socket("127.0.0.1", SOCKET_PORT);
+        LOGGER.info("Iniciando socket.");
+        while (true) {
+            try {
+                OutputStream os = sock.getOutputStream();
+                byte[] logBytes = LogStack.stack.pop().getBytes();
+                os.write(logBytes, 0, logBytes.length);
+                os.flush();
+            } catch (EmptyStackException e) {
+                continue;
+            } catch (IOException io) {
+                if (ioExceptionErrorCount == 3) {
+                    LOGGER.error("Erro ao pegar OutputStream da socket. \n{}", io);
                 }
+                ioExceptionErrorCount++;
+                continue;
             }
-        }catch (Exception e){
-            LOGGER.error("Erro de socket {}", e);
         }
     }
 }
