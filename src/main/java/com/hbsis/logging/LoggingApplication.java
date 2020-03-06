e58e5d5381498d4e59861af58c63eff54b411c58
matheus.toniotti <matheus.toniotti@gmail.com>
2020-03-06 14:57:40 -0300

    Socket server.

--- src/main/java/com/hbsis/logging/LoggingApplication.java
+++ src/main/java/com/hbsis/logging/LoggingApplication.java
@@ -1,13 +1,25 @@
 package com.hbsis.logging;
 
+import com.hbsis.logging.socket.SocketServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class LoggingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoggingApplication.class, args);
+		new Thread(() ->{
+			try {
+				SocketServer.streamFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 
 }
---
+++ src/main/java/com/hbsis/logging/socket/SocketServer.java
@@ -0,0 +1,40 @@
+package com.hbsis.logging.socket;
+
+import com.hbsis.logging.log.LogStack;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.EmptyStackException;
+
+@Service
+public abstract class SocketServer {
+
+    public static final int SOCKET_PORT = 5501;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SocketServer.class);
+
+    public static void streamFile() throws IOException, InterruptedException {
+        int ioExceptionErrorCount = 0;
+
+        try (Socket sock = new Socket("127.0.0.1", SOCKET_PORT)) {
+            while (true) {
+                try (OutputStream os = sock.getOutputStream()) {
+                    byte[] logBytes = LogStack.stack.pop().getBytes();
+                    os.write(logBytes, 0, logBytes.length);
+                    os.flush();
+                } catch (EmptyStackException e) {
+                    continue;
+                }catch (IOException io){
+                    if(ioExceptionErrorCount == 3){LOGGER.error("Erro ao pegar OutputStream da socket. \n{}", io); break;}
+                    ioExceptionErrorCount++;
+                    Thread.sleep(1000);
+                    continue;
+                }
+            }
+        }
+    }
+}
